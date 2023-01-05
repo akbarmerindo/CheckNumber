c6a405d1d3c18e540690353f7d1e5dbebc141d93
akbarmerindo <akbarmerindo123@gmail.com>
2023-01-05 14:31:41 +0700

    commit message

---
+++ src/main/java/com/mycompany/numbercheck/Menu.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.numbercheck;
+import java.util.Scanner; 
+public class Menu 
+{ 
+ public void Menu() 
+ { 
+ int option; 
+ Scanner sc = new Scanner(System.in); 
+ System.out.println("---------Menu---------"); 
+ System.out.println("1. Check Odd or Even"); 
+ System.out.println("2. Exit"); 
+ System.out.print("\nChoose the option: "); 
+ 
+ option = sc.nextInt(); 
+ 
+ // Switch Case 
+ 
+ switch(option) 
+ { 
+ case 1: CheckOddOrEven();
+ break; 
+ case 2: Exit(); 
+ break; 
+ default: System.out.println("Incorrect menu option"); 
+ Menu(); 
+ break; 
+ } 
+ 
+ } 
+ // Methods definition 
+ public void CheckOddOrEven() 
+ { 
+ OddOrEven obj = new OddOrEven(); 
+ obj.CheckOddOrEven(); 
+ } 
+ public void Exit() 
+ { 
+ System.exit(0); 
+ } 
+public static void main(String[] args) 
+ { 
+ Menu obj = new Menu(); 
+ obj.Menu(); 
+ } 
+ }
